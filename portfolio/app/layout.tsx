6cef489c0309b6b8c8ece9755ac0812075ba2510
Wylliam <wylliam.cantin-charawi@ens.etsmtl.ca>
2025-12-07 14:01:20 -0500

    refactor: add scroll-smooth class to html element for improved scrolling behavior

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html className="scroll-smooth" suppressHydrationWarning lang="en">
       <head />
       <body
         className={clsx(
